4371fccf1c9fd013ffd053c8f1a9726e1a58920c
Agapova.E <Agapova.E@106PC0124>
2020-03-12 10:23:39 +0300

    added some comments

--- PythonTask11.py
+++ PythonTask11.py
@@ -3,42 +3,41 @@ import fdb
 import pymssql
 import sys
 
-con = fdb.create_database("create database '127.0.0.1: C:\Program Files (x86)\Firebird\Firebird_3_0\FBBase.fb' user 'sysdba' password 'masterkey'")
+# *создаем БД ФБ
+#con = fdb.create_database("create database '127.0.0.1: C:\Program Files (x86)\Firebird\Firebird_3_0\FBBase.fb' user 'sysdba' password 'masterkey'")
+
+# коннектимся к БД ФБ
 con = fdb.connect(host='127.0.0.1', database='C:\Program Files (x86)\Firebird\Firebird_3_0\FBBase.fb', user='sysdba', password='masterkey', charset='UTF8')
 cur = con.cursor()
 
-# создаем таблицу с именами, заполняем
-cur.execute("create table FB_NAME (ID_NAME int, NAME varchar(50))")
-con.commit()
-cur.execute("create unique index NAME_ID on FB_NAME(ID_NAME)")
-con.commit()
+# *создаем таблицу с именами, заполняем
+#cur.execute("create table FB_NAME (ID_NAME int, NAME varchar(50))")
+#con.commit()
+#cur.execute("create unique index NAME_ID on FB_NAME(ID_NAME)")
+#con.commit()
+#cur.execute("insert into FB_NAME values (1 ,'Molly')")
+#cur.execute("insert into FB_NAME values (3 ,'Ronnie')")
+#cur.execute("insert into FB_NAME values (2 ,'Ricardo')")
+#con.commit()
 
-# заполняем
-cur.execute("insert into FB_NAME values (1 ,'Molly')")
-cur.execute("insert into FB_NAME values (3 ,'Ronnie')")
-cur.execute("insert into FB_NAME values (2 ,'Ricardo')")
-con.commit()
+# *создаем таблицу с фамилиями, заполняем
+#cur.execute("create table FB_SURNAME (ID_SURNAME int, SURNAME varchar(50))")
+#con.commit()
+#cur.execute("create unique index SURNAME_ID on FB_SURNAME(ID_SURNAME)")
+#con.commit()
+#cur.execute("insert into FB_SURNAME values (1, 'Rose')")
+#cur.execute("insert into FB_SURNAME values (3, 'Radke')")
+#cur.execute("insert into FB_SURNAME values (2, 'Milos')")
+#con.commit()
 
-# создаем таблицу с фамилиями
-cur.execute("create table FB_SURNAME (ID_SURNAME int, SURNAME varchar(50))")
-con.commit()
-cur.execute("create unique index SURNAME_ID on FB_SURNAME(ID_SURNAME)")
-con.commit()
-
-# заполняем
-cur.execute("insert into FB_SURNAME values (1, 'Rose')")
-cur.execute("insert into FB_SURNAME values (3, 'Radke')")
-cur.execute("insert into FB_SURNAME values (2, 'Milos')")
-con.commit()
-
-#смотрим содержимое таблиц
+# *проверяем содержимое таблиц
 #cur.execute("select * FROM FB_NAME order by ID_NAME")
 #print(cur.fetchall())
 #cur.execute("select * FROM FB_SURNAME order by ID_SURNAME")
 #print(cur.fetchall())
 
 try:
-    #если есть
+    #если файлы уже есть
     f = open('NameFile.txt', mode='r')
     g = open('SurnameFile.txt', mode='r')
     f.close()
@@ -67,11 +66,11 @@ con.close()
 con = pymssql.connect(server='106PC0124\PT66OLCI2450', user='***', password='***', database='MSSQLBase')
 cur = con.cursor()
 
-# cоздаём таблицу в MS SQL
-cur.execute("create table People (ID_People int, NAME varchar(50), SURNAME varchar(50))")
-con.commit()
-cur.execute("create unique index PEOPLE_ID on People (ID_People)")
-con.commit()
+# *cоздаём таблицу в MS SQL
+#cur.execute("create table People (ID_People int, NAME varchar(50), SURNAME varchar(50))")
+#con.commit()
+#cur.execute("create unique index PEOPLE_ID on People (ID_People)")
+#con.commit()
 
 # достаём данные из файлов
 f = open('NameFile.txt', mode='r')
